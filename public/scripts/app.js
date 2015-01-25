c24592f931d4b805563911bc3810ec2a3f55e742
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2015-01-25 15:59:42 +0100

    add simple visualization using Chartist

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,50 @@
 (function(angular) {
     'use strict';
 
-    var module = angular.module('app', ['angular-chartist']);
+    var module = angular.module('kasperbrettApp', ['angular-chartist']);
 
-    module.controller('ChartistExampleCtrl', ['$scope', '$interval',
-        function($scope, $interval) {
+    module.controller('MainCtrl', function($scope, $interval, $http) {
+
+        this.dataSources = [];
+
+        // TODO: extract data access to a service
+        $http.get('/api/datasources?include-data=1').then(function(res) {
+            res.data.forEach(function(dataSource) {
+                var labels = [];
+                dataSource.labels.forEach(function(timestampMillis) {
+                    var date = new Date(timestampMillis);
+                    labels.push(
+                        formatDateComponent(date.getHours()) + ':' + formatDateComponent(date.getMinutes()) + ':' + formatDateComponent(date.getSeconds())
+                    );
+                })
+
+                dataSource.chartData = {
+                    labels: labels,
+                    series: [dataSource.series]
+                }
+                delete dataSource.labels;
+                delete dataSource.series
+
+                this.dataSources.push(dataSource);
+            }.bind(this))
+
+            console.log('res.data', this.dataSources);
+
+
+
+        }.bind(this));
+
+
+        function formatDateComponent(dateComponent) {
+            var returnVal = dateComponent;
+
+            if (returnVal < 10) {
+                returnVal = '0' + returnVal;
+            }
+
+            return returnVal;
+        }
+
 
             this.events = {
                 draw: function(obj) {
@@ -50,22 +90,14 @@
 
             // line chart
             this.lineData = {
-                labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+                labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
                 series: [
-                    [0, 1, 2, 4, 7, 6, 9, 10, 8, 10, 14, 13, 16, 14, 17, 19, 20, 31, 32, 26, 36, 28, 31, 40, 26, 26, 43, 47, 55, 30],
+                    ["3152", "3152", "3152", "3152", "3152", "3152", "3152", "3152", "3152", "3152"]/*,
                     [0, 1, 2, 4, 4, 6, 6, 13, 9, 10, 16, 18, 21, 16, 16, 16, 31, 17, 27, 23, 31, 29, 35, 39, 30, 32, 26, 43, 51, 46],
                     [0, 1, 3, 4, 6, 5, 11, 9, 11, 11, 13, 15, 14, 22, 20, 15, 31, 27, 25, 25, 36, 30, 37, 29, 29, 39, 40, 49, 34, 35],
                     [0, 1, 3, 5, 7, 5, 9, 9, 10, 17, 13, 21, 14, 16, 23, 23, 25, 17, 24, 34, 27, 39, 33, 45, 47, 32, 40, 36, 49, 32],
-                    [0, 1, 3, 3, 7, 5, 8, 11, 12, 13, 16, 17, 20, 24, 27, 15, 22, 33, 35, 24, 32, 35, 41, 39, 24, 31, 51, 29, 45, 50]
+                    [0, 1, 3, 3, 7, 5, 8, 11, 12, 13, 16, 17, 20, 24, 27, 15, 22, 33, 35, 24, 32, 35, 41, 39, 24, 31, 51, 29, 45, 50]*/
                 ]
-            };
-
-            this.lineOptions = {
-                axisX: {
-                    labelInterpolationFnc: function(value) {
-                        return value;
-                    }
-                }
             };
 
             // pie chart
@@ -78,7 +110,7 @@
                 donut: true
             };
 
-            function pushLimit(arr, elem, limit) {
+            /*function pushLimit(arr, elem, limit) {
                 arr.push(elem);
                 if (arr.length > limit) {
                     arr.splice(0, 1);
@@ -103,8 +135,7 @@
             // Cancel interval once scope is destroyed
             $scope.$on('$destroy', function() {
                 $interval.cancel(barUpdatePromise);
-            });
-        }
-    ]);
+            });*/
+    });
 
 })(angular);
